82d433d734318d81a3675f6a0931ac6fe84aba40
Simon <simon.chaffart@gmail.com>
2016-12-15 18:18:37 +0100

    Initial commit

---
+++ src/be/chaffart/GitProjectMain.java
@@ -0,0 +1,10 @@
+package be.chaffart;
+
+/**
+ * Created by Simon on 15/12/2016.
+ */
+public class GitProjectMain {
+    public static void main (String[] args){
+        System.out.println("GIT PROJECT:");
+    }
+}
